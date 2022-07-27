b9dcc232834965d3f2c4af8319255e427854c724
mariahlu17 <62620965+mariahlu17@users.noreply.github.com>
2022-07-27 20:16:01 +0300

    Add files via upload
    adding new pages of webApp

---
+++ webApp/setting.js
@@ -0,0 +1,11 @@
+
+const element =document.querySelector(".dropdown-el")
+
+
+element.click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $(this).toggleClass('expanded');
+    $('#'+$(e.target).attr('for')).prop('checked',true);
+  
+  });
